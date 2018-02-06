bb73043c0aa846bcf959ecc00297feca73faca38
OvermindDL1 <overminddl1@gmail.com>
2018-02-06 16:47:45 -0700

    Format: lib/overdiscord.ex
    Yay a case where it fixed emacs weird right indentation!

--- lib/overdiscord.ex
+++ lib/overdiscord.ex
@@ -14,21 +14,24 @@ defmodule Overdiscord do
       _ -> :ok
     end
 
-    #{:ok, client} = ExIrc.start_link!()
+    # {:ok, client} = ExIrc.start_link!()
 
     children = [
       worker(Overdiscord.Cron, []),
       worker(Overdiscord.IRC.Bridge, []),
       worker(Alchemy.Client, [System.get_env("OVERDISCORD_TOKEN"), []]),
       worker(Overdiscord.Commands, []),
-      worker(Cachex, [:summary_cache, [
-                         fallback: &Overdiscord.SiteParser.get_summary_cache_init/1,
-                         #default_ttl: :timer.hours(24),
-                         disable_ode: true,
-                         #ttl_interval: :timer.hours(1),
-                         limit: %Cachex.Limit{limit: 10000, reclaim: 0.1},
-                         record_stats: true,
-                       ]]),
+      worker(Cachex, [
+        :summary_cache,
+        [
+          fallback: &Overdiscord.SiteParser.get_summary_cache_init/1,
+          # default_ttl: :timer.hours(24),
+          disable_ode: true,
+          # ttl_interval: :timer.hours(1),
+          limit: %Cachex.Limit{limit: 10000, reclaim: 0.1},
+          record_stats: true
+        ]
+      ])
     ]
 
     opts = [strategy: :one_for_one, name: Overdiscord.Supervisor, restart: :permanent]
