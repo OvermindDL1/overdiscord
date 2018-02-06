e73c4018cabc5007ada6e404266a2a517da203ce
OvermindDL1 <overminddl1@gmail.com>
2018-02-06 16:37:07 -0700

    Format: lib/commands.ex
    Don't like the small function heads being separated so far.
    *Really* do not like the right aligned cases in the anonymous functions,
    should only be indented by a single indent, not aaaaaall the way over.

--- lib/commands.ex
+++ lib/commands.ex
@@ -1,26 +1,43 @@
 defmodule Overdiscord.Commands do
-
   use Alchemy.Events
 
   def on_msg(%{author: %{id: "336892378759692289"}}) do
     # Ignore bot message
     :ok
   end
-  def on_msg(%{author: %{bot: _true_or_false, username: username}, channel_id: "320192373437104130", content: content}=msg) do
+
+  def on_msg(
+        %{
+          author: %{bot: _true_or_false, username: username},
+          channel_id: "320192373437104130",
+          content: content
+        } = msg
+      ) do
     case content do
-      "!list" -> Overdiscord.IRC.Bridge.list_users()
-      "!"<>_ -> :ok
+      "!list" ->
+        Overdiscord.IRC.Bridge.list_users()
+
+      "!" <> _ ->
+        :ok
+
       content ->
-        #IO.inspect("Msg dump: #{inspect msg}")
+        # IO.inspect("Msg dump: #{inspect msg}")
         IO.inspect("Sending message from Discord to IRC: #{username}: #{content}")
         irc_content = get_msg_content_processed(msg)
         Overdiscord.IRC.Bridge.send_msg(username, irc_content)
-        Enum.map(msg.attachments, fn %{filename: filename, size: size, url: url, proxy_url: _proxy_url}=_attachment ->
+
+        Enum.map(msg.attachments, fn %{
+                                       filename: filename,
+                                       size: size,
+                                       url: url,
+                                       proxy_url: _proxy_url
+                                     } = _attachment ->
           size = Sizeable.filesize(size, spacer: "")
           Overdiscord.IRC.Bridge.send_msg(username, "#{filename} #{size}: #{url}")
         end)
     end
   end
+
   def on_msg(msg) do
     IO.inspect(msg, label: :UnhandledMsg)
   end
@@ -28,12 +45,20 @@ defmodule Overdiscord.Commands do
   def on_msg_edit(%{author: %{id: "336892378759692289"}}) do
     # Ignore bot message
   end
-  def on_msg_edit(%{author: %{bot: _true_or_false, username: username} = author, channel_id: "320192373437104130", content: content}=msg) do
+
+  def on_msg_edit(
+        %{
+          author: %{bot: _true_or_false, username: username} = author,
+          channel_id: "320192373437104130",
+          content: content
+        } = msg
+      ) do
     case content do
       "!" <> _ -> :ok
       _content -> on_msg(%{msg | author: %{author | username: "#{username}{EDIT}"}})
     end
   end
+
   def on_msg_edit(msg) do
     IO.inspect(msg, label: :EditedMsg)
   end
@@ -48,11 +73,17 @@ defmodule Overdiscord.Commands do
     use Overdiscord.Commands.GD
     Alchemy.Cogs.EventHandler.add_handler({:message_create, {__MODULE__, :on_msg}})
     Alchemy.Cogs.EventHandler.add_handler({:message_update, {__MODULE__, :on_msg_edit}})
+
     spawn(fn ->
       Process.sleep(5000)
       # Load entire userlist, at a rate of 100 per minutes because of discord limits
-      Alchemy.Cache.load_guild_members(elem(Alchemy.Cache.guild_id(Overdiscord.IRC.Bridge.alchemy_channel()), 1), "", 0)
+      Alchemy.Cache.load_guild_members(
+        elem(Alchemy.Cache.guild_id(Overdiscord.IRC.Bridge.alchemy_channel()), 1),
+        "",
+        0
+      )
     end)
+
     {:ok, nil}
   end
 
@@ -63,22 +94,27 @@ defmodule Overdiscord.Commands do
       {:error, reason} ->
         IO.inspect("Unable to process guild_id: #{reason}\n\t#{msg}")
         content
+
       {:ok, guild_id} ->
         Regex.replace(~r/<@([0-9!]+)>/, content, fn full, user_id ->
           case Alchemy.Cache.member(guild_id, user_id) do
             {:ok, %Alchemy.Guild.GuildMember{user: %{username: username}}} ->
               "@#{username}"
+
             v ->
               case Alchemy.Client.get_member(guild_id, user_id) do
                 {:ok, %Alchemy.Guild.GuildMember{user: %{username: username}}} ->
                   "@#{username}"
+
                 err ->
-                  IO.inspect("Unable to get member of guild: #{inspect v}\n\tError: #{inspect err}")
+                  IO.inspect(
+                    "Unable to get member of guild: #{inspect(v)}\n\tError: #{inspect(err)}"
+                  )
+
                   full
               end
           end
         end)
     end
   end
-
 end
