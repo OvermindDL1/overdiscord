51ee627d3469c216b8abb48f5801225d74a20c0a
OvermindDL1 <overminddl1@gmail.com>
2018-02-06 16:33:17 -0700

    Format: lib/leveldb_protocols.ex
    Good, I don't know why emacs formatted it the way it originally did...

--- lib/leveldb_protocols.ex
+++ lib/leveldb_protocols.ex
@@ -1,13 +1,13 @@
 for f <- [Map, Tuple, Any] do
-    defimpl Exleveldb.Keys, for: f do
-      def to_key(value) do
-        :erlang.term_to_binary(value)
-      end
+  defimpl Exleveldb.Keys, for: f do
+    def to_key(value) do
+      :erlang.term_to_binary(value)
     end
+  end
 
-    defimpl Exleveldb.Values, for: f do
-      def to_value(value) do
-        :erlang.term_to_binary(value)
-      end
+  defimpl Exleveldb.Values, for: f do
+    def to_value(value) do
+      :erlang.term_to_binary(value)
     end
+  end
 end
