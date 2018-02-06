67695ff0159dd67257cd8e88beb2f3290a38f0a8
OvermindDL1 <overminddl1@gmail.com>
2018-02-06 16:41:58 -0700

    Format: lib/commands/gt6.ex
    The formatter seems to sometimes put function heads on the same line as
    the `=` before it, and sometimes after, what is the reasoning...
    OMG why is it moving comments out of pipe calls?!?  What is going on?!?
    They have lost all context of where they belong now!
    Annoying lack of trailing commas in multi-line lists/tuples/maps still...
    Yay it is fixing some of emacs *weird* indentation choices though!
    It should do 2 blank lines between major function head sections though
    (and 3 blank lines between major overall sections...), instead it's
    doing only 1, like everywhere...
    Why is it removing lists around final arguments in functions when it is
    not appropriate for the given context...  Blah...

--- lib/commands/gt6.ex
+++ lib/commands/gt6.ex
@@ -4,115 +4,137 @@ defmodule Overdiscord.Commands.GT6 do
   import Embed
 
   # @red_embed %Embed{color: 0xd44480}
-  @blue_embed %Embed{color: 0x1f95c1}
+  @blue_embed %Embed{color: 0x1F95C1}
 
   Cogs.group("gt6")
 
   def get_changelog() do
-    %{body: changelog, headers: headers} = HTTPoison.get!("http://gregtech.overminddl1.com/com/gregoriust/gregtech/gregtech_1.7.10/changelog.txt")
+    %{body: changelog, headers: headers} =
+      HTTPoison.get!(
+        "http://gregtech.overminddl1.com/com/gregoriust/gregtech/gregtech_1.7.10/changelog.txt"
+      )
+
+    # Remove the top message about how often the changelog is updated
     changelog =
       String.split(changelog, "\r\n\r\n\r\n", trim: true)
-      |> tl() # Remove the top message about how often the changelog is updated
+      |> tl()
       |> Enum.map(fn version_entry ->
-          version_entry
-          |> String.split("\r\n[", trum: true)
-          |> case do
-               [_] -> []
-               [version_line | entries] ->
-                {version, description} =
-                   case String.split(version_line, ":", parts: 2) do
-                     [version] -> {version, ""}
-                     [version, " (Not released yet)"] -> {version, "(Not released yet)"}
-                     [version, description] -> {version, description}
-                   end
-                 entries =
-                   entries
-                   |> Enum.map(fn entry ->
-                       case String.split(entry, "]", parts: 2) do
-                         [reason, description] -> {reason, String.trim(description)}
-                         _ -> entry
-                       end
-                   end)
-                 %{
-                   version: version,
-                   description: description,
-                   entries: entries,
-                   url: "http://gregtech.overminddl1.com/com/gregoriust/gregtech/gregtech_1.7.10/changelog.txt",
-                 }
-             end
-    end)
-    |> List.flatten()
-    last_updated =
-        case List.keyfind(headers, "Last-Modified", 0) do
-          nil -> "Unknown"
-          {_, date} -> date
+        version_entry
+        |> String.split("\r\n[", trum: true)
+        |> case do
+          [_] ->
+            []
+
+          [version_line | entries] ->
+            {version, description} =
+              case String.split(version_line, ":", parts: 2) do
+                [version] -> {version, ""}
+                [version, " (Not released yet)"] -> {version, "(Not released yet)"}
+                [version, description] -> {version, description}
+              end
+
+            entries =
+              entries
+              |> Enum.map(fn entry ->
+                case String.split(entry, "]", parts: 2) do
+                  [reason, description] -> {reason, String.trim(description)}
+                  _ -> entry
+                end
+              end)
+
+            %{
+              version: version,
+              description: description,
+              entries: entries,
+              url:
+                "http://gregtech.overminddl1.com/com/gregoriust/gregtech/gregtech_1.7.10/changelog.txt"
+            }
         end
+      end)
+      |> List.flatten()
+
+    last_updated =
+      case List.keyfind(headers, "Last-Modified", 0) do
+        nil -> "Unknown"
+        {_, date} -> date
+      end
+
     %{
       changesets: changelog,
-      last_updated: last_updated,
+      last_updated: last_updated
     }
   end
 
-
   def get_changelog_version(version \\ nil)
+
   def get_changelog_version(nil) do
     %{changesets: changesets, last_updated: last_updated} = get_changelog()
     {:ok, %{changesets: [List.first(changesets)], last_updated: last_updated}}
   end
+
   def get_changelog_version("latest"), do: get_changelog_version(nil)
   def get_changelog_version(""), do: get_changelog_version(nil)
+
   def get_changelog_version("" <> version) do
     %{changesets: changesets, last_updated: last_updated} = get_changelog()
-    case Enum.find(changesets, nil, &(&1.version===version)) do
+
+    case Enum.find(changesets, nil, &(&1.version === version)) do
       nil ->
         possibles =
           changesets
           |> Enum.map(&{&1.version, String.jaro_distance(&1.version, version)})
           |> Enum.sort(&(elem(&1, 1) >= elem(&2, 1)))
-          |> Enum.filter(&(elem(&1, 1)>=0.1))
+          |> Enum.filter(&(elem(&1, 1) >= 0.1))
           |> Enum.take(4)
           |> Enum.map(&elem(&1, 0))
           |> Enum.intersperse(" | ")
           |> Enum.join()
           |> case do
-               [] ->
-                 changesets
-                 |> Enum.take(4)
-                 |> Enum.map(&(&1.version))
-                 |> Enum.intersperse(" | ")
-                 |> Enum.join()
-               possibles -> possibles
-             end
-          {:error,
-            [
-              "Version `#{version}` is not found, maybe you meant to run the command with no parameters, or perhaps you meant one of these versions:",
-              "\t#{possibles}"
-            ]
-          }
-      changeset -> {:ok, %{changesets: [changeset], last_updated: last_updated}}
+            [] ->
+              changesets
+              |> Enum.take(4)
+              |> Enum.map(& &1.version)
+              |> Enum.intersperse(" | ")
+              |> Enum.join()
+
+            possibles ->
+              possibles
+          end
+
+        {:error,
+         [
+           "Version `#{version}` is not found, maybe you meant to run the command with no parameters, or perhaps you meant one of these versions:",
+           "\t#{possibles}"
+         ]}
+
+      changeset ->
+        {:ok, %{changesets: [changeset], last_updated: last_updated}}
     end
   end
 
-
   def format_changelog_as_text(changelog)
   def format_changelog_as_text(%{changesets: []}), do: []
+
   def format_changelog_as_text(%{changesets: changesets, last_updated: last_updated}) do
     changeset = hd(changesets)
+
     [
       "Gregtech 6 Changelog: #{changeset.version}",
       changeset.description,
       List.foldr(changeset.entries, [], fn
         {type, entry}, acc -> [type <> ": " <> entry | acc]
         entry, acc -> [entry | acc]
-      end),
-    ] |> List.flatten(["Last Updated On: #{last_updated}"])
+      end)
+    ]
+    |> List.flatten(["Last Updated On: #{last_updated}"])
   end
-
 
   def format_changelog_as_embed(changeset)
   def format_changelog_as_embed(%{changesets: []}), do: nil
+
   def format_changelog_as_embed(%{changesets: changesets, last_updated: last_updated}) do
     changeset = hd(changesets)
+
     embed =
       Enum.reduce(changeset.entries, @blue_embed, fn
         entry, embed when is_binary(entry) -> field(embed, "", entry)
@@ -120,11 +142,11 @@ defmodule Overdiscord.Commands.GT6 do
       end)
       |> title("Gregtech 6 Changelog: #{changeset.version}")
       |> url(changeset.url)
-      |> footer([text: "Last Updated On: #{last_updated}"])
+      |> footer(text: "Last Updated On: #{last_updated}")
+
     embed = if(changeset.description, do: description(embed, changeset.description), else: embed)
     embed
   end
-
 
   # def send_changeset(message, version \\ nil) do
   #   Client.trigger_typing(message.channel_id)
@@ -175,6 +197,7 @@ defmodule Overdiscord.Commands.GT6 do
 
   def send_changeset(message, version \\ nil) do
     Client.trigger_typing(message.channel_id)
+
     case get_changelog_version(version) do
       {:error, msgs} -> Cogs.say(Enum.join(msgs, "\n"))
       {:ok, %{changesets: []}} -> Cogs.say("No changesets in changelog")
@@ -186,7 +209,7 @@ defmodule Overdiscord.Commands.GT6 do
     send_changeset(message)
   end
 
-  Cogs.def changelog version do
+  Cogs.def changelog(version) do
     send_changeset(message, version)
   end
 end
